5457a06fd61a1714d029d043032c3e49734d2605
alu0101333588 <alu0101333588@ull.edu.es>
2024-11-22 11:08:52 +0000

    actualizacion

--- server/eliminacion.js
+++ server/eliminacion.js
@@ -23,7 +23,7 @@ mongoose.connect(MONGODB_URI, {
 // Función para eliminar la colección 'usuarios'
 async function eliminarColeccionUsuarios() {
   try {
-    await mongoose.connection.db.collection('citas').drop();
+    await mongoose.connection.db.collection('formContacto').drop();
     console.log('Colección "usuarios" eliminada exitosamente');
   } catch (error) {
     if (error.code === 26) {
