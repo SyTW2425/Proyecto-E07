98e29dfeb913b0eaa5d6ca850866805cb2b4113e
alu0101333588 <alu0101333588@ull.edu.es>
2025-01-08 17:24:20 +0000

    modificadas rutas recetas e informes

---
+++ client/src/components/RecetasWrapper.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+      <div v-if="loading">Cargando...</div>
+      <div v-else>
+        <RecetasMedico v-if="userType === 'Médico'" />
+        <Recetas v-if="userType === 'Paciente'" />
+        <div v-if="userType === 'Desconocido'">
+          Tipo de usuario no reconocido
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { useAuthStore } from '../../store/auth';
+  import RecetasMedico from './RecetasMedico.vue';
+  import Recetas from './Recetas.vue';
+  
+  export default {
+    data() {
+      return {
+        userType: null, // Tipo de usuario
+        loading: true,  // Indicador de carga mientras se determina el tipo
+      };
+    },
+    components: {
+      RecetasMedico,
+      Recetas,
+
+    },
+    async created() {
+      const authStore = useAuthStore();
+  
+      if (authStore.isAuthenticated) {
+        console.log('Usuario autenticado:', authStore.getUser);
+        const tipo = authStore.getUser ? authStore.getUser.tipo : null;
+        if (['Paciente', 'Médico'].includes(tipo)) {
+          this.userType = tipo;
+        } else {
+          this.userType = 'Desconocido';
+          setTimeout(() => {
+            this.$router.push('/login');
+          }, 30000); // Redirige a /login después de 30 segundos
+        }
+        this.loading = false; // Se indica que terminó de cargar
+      } else {
+        console.log('Usuario no autenticado');
+        // Redirige a la página anterior o al login como respaldo
+        if (window.history.length > 1) {
+          this.$router.go(-1);
+        } else {
+          this.$router.push('/login');
+        }
+      }
+    }
+  };
+  </script>
